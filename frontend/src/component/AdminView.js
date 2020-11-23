953d4a6cad8c8db7e4f5d7ff290fff1b07bff5f9
Nagy Milán <nagymilan1008@gmail.com>
2020-11-23 10:10:09 +0100

    Admin View migrated to formik

--- frontend/src/component/AdminView.js
+++ frontend/src/component/AdminView.js
@@ -1,19 +1,82 @@
 import React from "react";
-import Login from "./Login";
-class AdminView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    }
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-}}
+import {Formik} from "formik";
+import * as Yup from "yup";
+import { parse, isDate } from "date-fns";
+function parseDateString(value, originalValue) {
+    const parsedDate = isDate(originalValue)
+        ? originalValue
+        : parse(originalValue, "yyyy-MM-dd", new Date());
+
+    return parsedDate;
+}
+const AdminView = () => (
+    <Formik
+        initialValues={{Name: "", ID: "", DateOfBirth: ""}}
+        onSubmit={(values, {setSubmitting}) => {
+            setTimeout(() => {
+                //LoginActions.login(values);
+                console.log("Ámen");
+                setSubmitting(false);
+            }, 500);
+        }}
+
+        validationSchema={Yup.object().shape({
+            Name: Yup.string()
+                .required("Név megadása kötelező"),
+            ID: Yup.number()
+                .required("Nem lehet üres!").positive().integer(),
+            DateOfBirth: Yup.string()
+                .required("Kötelező megadni!")
+        })}
+    >
+        {props => {
+            const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit
+            } = props;
+            return (
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            name="email"
+                            type="text"
+                            placeholder="Add meg az email címed"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={ errors.email && touched.email ? "form-control is-invalid" : "form-control"}
+                        />
+                        {errors.email && touched.email && (
+                            <div className={"invalid-feedback"}>{errors.email} </div>
+                        )}
+                    </div>
+                    <div className={"form-group"}>
+                        <label htmlFor="email">ID</label>
+                        <input
+                            name="ID"
+                            type="text"
+                            placeholder="Add meg az ID-t"
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={errors.password && touched.password ? "form-control is-invalid" : "form-control"}
+                        />
+                        {errors.password && touched.password && (
+                            <div className={"invalid-feedback"}>{errors.password}</div>
+                        )}
+                    </div>
+                    <button type="submit" disabled={isSubmitting} className={"btn btn-primary"}>
+                        Check in
+                    </button>
+                </form>
+            );
+        }}
+    </Formik>
+);
 export default AdminView;
