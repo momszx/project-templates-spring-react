231e659e21f852000034a39f0b6d4aff954cabf9
Heim József <mrszenya@gmail.com>
2020-11-23 18:51:43 +0100

    #39 leaving oldal faker

--- frontend/backend/fakeBackend.js
+++ frontend/backend/fakeBackend.js
@@ -52,6 +52,66 @@ server.get('/login',(req,res) => {
         res.status(httpStatus.OK).send(user);
     }
 });
+
+server.post('/leaving',(req,res) => {console.log(req.body);
+    res.status(200).send(
+        {
+            id: 'id'
+        }
+    );
+});
+
+server.get('/leaving',(req,res) => {
+    const guests = [
+        {
+            id: 1,
+            name: 'Gipsz Jakab',
+            dateOfBirth: '1988-09-29',
+            arrivalDate: '2020-11-12-12-00-00',
+            departureDate: '2020-11-12-13-00-00',
+            usedServices: [{
+                id: '1',
+                service: 'vizicsúszda',
+                barCode: ''
+            },
+                {
+                    id: '2',
+                    service: '',
+                    barCode: 'cocacola'
+                }]
+        },
+        {
+            id: 2,
+            name: 'Gipsz Jakab Jr',
+            dateOfBirth: '2010-09-29',
+            arrivalDate: '2020-11-12-12-00-00',
+            departureDate: '2020-11-12-13-00-00',
+            usedServices: [{
+                id: '3',
+                service: 'vizicsúszda',
+                barCode: ''
+            },
+                {
+                    id: '4',
+                    service: '',
+                    barCode: 'cocacola'
+                }]
+        }
+    ];
+    const {
+        id
+    } = req.query;
+    let guest = guests.find((tmp) => {
+        return tmp.id === parseInt(id)
+    });
+    if(guest === undefined){
+        res.status(httpStatus.UNAUTHORIZED).send();
+    }
+    else{
+        res.status(httpStatus.OK).send(guest);
+    }
+});
+
 server.use(temp);
 server.use(router);
 server.listen(8080,function (){
