ffde8ffce5bc52fb76af04871d57b82df3494ce3
momsz <donkomark@gmail.com>
2020-11-25 15:42:25 +0100

    Import hiba javítások

--- backend/src/main/java/hu/uni/eku/team3/service/GuestService.java
+++ backend/src/main/java/hu/uni/eku/team3/service/GuestService.java
@@ -1,6 +1,6 @@
 package hu.uni.eku.team3.service;
 
-import hu.uni.eku.team3.model.Products;
+import hu.uni.eku.team3.model.Guest;
 import hu.uni.eku.team3.service.exceptions.*;
 
 import java.util.Collection;
--- backend/src/main/java/hu/uni/eku/team3/service/ProductsService.java
+++ backend/src/main/java/hu/uni/eku/team3/service/ProductsService.java
@@ -6,8 +6,5 @@ import hu.uni.eku.team3.service.exceptions.*;
 import java.util.Collection;
 
 public interface ProductsService {
-    void create(Products productCreate) throws ProductAlreadyExistsException;
     Collection<Products> readAll();
-    void update(Integer original, Products updated) throws ProductNotFoundException;
-    void delete(Integer productDelete) throws ProductNotFoundException;
 }
